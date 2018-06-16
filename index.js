fde74e23a37d20787c25d5cfb3dc95023ad22b47
John Stratton <jstrat0407@gmail.com>
2018-06-16 13:24:59 -0400

    planned out code with comments

---
+++ index.js
@@ -0,0 +1,27 @@
+// picks a random number
+
+// user gets 5 guesses
+
+  // let user know if they guessed correctly
+  
+    // user wins the game
+
+      // how many guesses it took
+
+  // let user know if they guessed incorrectly
+
+    // guess was too low
+
+      // how many guesses are left
+
+    // guess was too high
+
+      // how many guesses are left
+
+    // if user is out of guesses
+
+      // user loses the game
+
+      // give the correct number
+
+// let user try again or end
